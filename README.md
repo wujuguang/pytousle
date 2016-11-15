c38276177810235c9bb684d015853239c1c7ee1b
wujuguang <1154545932@qq.com>
2016-11-15 18:07:47 +0800

    Fixed.

--- README.md
+++ README.md
@@ -2,12 +2,19 @@
 顾名思义, 存放的是杂乱无章的小代码, 在兵荒马乱的日子.
 
 ### 索引:
-    Issue:
-        1. issue/get_repeat_data.py: 从list对象中取出重复数据的几种方式.
-        2. issue/get_round_number.py: 从float对象中向上向下取出整几种方式.
-        3. issue/merge_sorted_array.py: 合并两个已排序列表.
-        4. issue/loop_single_linked_list.py: 单链表遍历.
+    Issue:        
+        issue/fibonacci.py: 斐波那契数列.
+        issue/get_prime_range.py: 从指定范围获取素数.
+        issue/get_repeat_data.py: 从list对象中取出重复数据的几种方式.
+        issue/get_round_number.py: 从float对象中向上向下取出整几种方式.
+        issue/get_days_from_date.py: 计算某日期为当前年中的第几天.
+        issue/merge_sorted_array.py: 合并两个已排序列表.
+        issue/loop_single_linked_list.py: 单链表遍历.
 
     Search:
-        1. search/binary_search.py: Sequence Search.
-        2. search/sequence_search.py: Binary Search.
+        search/binary_search.py: Sequence Search.
+        search/sequence_search.py: Binary Search.
+        
+    Sort:
+        sort/bucket_sort.py: 桶排序.
+        sort/base/bubble_sort.py: 冒泡排序.
--- issue/get_repeat_data.py
+++ issue/get_repeat_data.py
@@ -9,7 +9,7 @@ Obtain the repeat data from a list object.
 
 
 def get_repeat_loop_cmp(lst):
-    """取出重复数据.
+    """取出重复数据(先排序,后遍历,比较相邻数).
 
         :param lst:
         :return:
@@ -21,14 +21,14 @@ def get_repeat_loop_cmp(lst):
     x, result = None, []
     lst = sorted(lst)
     for i in lst:
-        if x and i == x:
+        if x is not None and i == x:
             result.append(i)
         x = i
     return result
 
 
 def get_repeat_loop_count(lst):
-    """取出重复数据.
+    """取出重复数据(先去重,后遍历,使用count计算原序列中该元素数量).
 
         :param lst:
         :return:
@@ -46,7 +46,7 @@ def get_repeat_loop_count(lst):
 
 
 def get_repeat_loop_in(lst):
-    """取出重复数据.
+    """取出重复数据(小时候捡豆法,先遍历,往好坏两个容器分别扔).
 
         :param lst:
         :return:
@@ -64,13 +64,13 @@ def get_repeat_loop_in(lst):
     return repeat_lst
 
 
-if __name__ == '__main__':
-    demo_lst = [1, 2, 7, 2, 3, 4, 5, 5, 6]
+def test(data_lst):
+    print(get_repeat_loop_cmp(data_lst))
+    print(get_repeat_loop_count(data_lst))
+    print(get_repeat_loop_in(data_lst))
 
-    # print(get_repeat_loop_cmp(demo_lst))
-    # print(get_repeat_loop_count(demo_lst))
-    # print(get_repeat_loop_in(demo_lst))
 
+def measure():
     from timeit import repeat, default_timer
 
     print(repeat("get_repeat_loop_cmp(demo_lst)", "from __main__ import get_repeat_loop_cmp,demo_lst",
@@ -81,3 +81,9 @@ if __name__ == '__main__':
 
     print(repeat("get_repeat_loop_in(demo_lst)", "from __main__ import get_repeat_loop_in,demo_lst",
                  default_timer, 10, 10000))
+
+
+if __name__ == '__main__':
+    demo_lst = [1, 2, 7, 2, 3, 4, 5, 5, 6]
+    test(demo_lst)
+    # measure()
